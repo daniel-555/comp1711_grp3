c49151fc5581bef4b75b4380b71f38b0a2009470
Daniel Spiers <danjspiers@gmail.com>
2023-10-24 14:55:24 +0100

    Week 3 Session 1 Done

--- C_Bootcamp/week3/session1/worksheet1/question3.c
+++ C_Bootcamp/week3/session1/worksheet1/question3.c
@@ -4,23 +4,25 @@
 
 int main()
 {
-    char filename[] = "squares.dat";
+    char filename[] = "numbers.dat";
     FILE *file = open_file(filename, "r");
 
     int max_length = 100;
     char line_buffer[max_length];
 
-    int sum = 0, n = 0;
+    float sum = 0;
+    int n = 0;
 
-    while(fgets(line_buffer, max_length, file) != NULL)
+    while (fgets(line_buffer, max_length, file) != NULL)
     {
-        sum += atoi(line_buffer);
+        sum += atof(line_buffer);
         n++;
     }
+    fclose(file);
+    file = open_file(filename, "a");
 
     float mean = sum / n;
-
-    printf("The mean of the first %d squares is %.2f\n", n, mean);
+    fprintf(file, "%f\n", mean);
 
     fclose(file);
     return 0;
