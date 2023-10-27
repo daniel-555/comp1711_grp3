42847a3a4308e36abfe43ae02dbaa18b690761a7
Daniel Spiers <danjspiers@gmail.com>
2023-10-27 14:12:28 +0100

    Coursework Part 1 Complete

--- Coursework/Part_1/StepsTask1.c
+++ Coursework/Part_1/StepsTask1.c
@@ -10,7 +10,7 @@ typedef struct {
 } FITNESS_DATA;
 
 // Define any additional variables here
-const int numRecords = 234;
+const char fileName[] = "FitnessData_2023.csv";
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
@@ -41,14 +41,51 @@ void tokeniseRecord(const char *input, const char *delimiter,
 }
 
 // Complete the main function
-int main() {
-    char filename[] = "../FitnessData_2023.csv";
-    FILE *file = fopen(filename, "r");
+int main() { 
+    // Open file
+    FILE *file = fopen(fileName, "r");
     if (file == NULL)
     {
         perror("");
         return 1;
     }
 
+    int numRecords = 0;
 
+    int maxLength = 100;
+    char lineBuffer[maxLength];
+
+    while(fgets(lineBuffer, maxLength, file) != NULL)
+    {
+        numRecords++;
+    }
+
+    printf("Number of records in file: %d\n", numRecords);
+    // Reset the pointer to the 1st line of the file
+    rewind(file);
+
+    FITNESS_DATA fitnessData[numRecords];
+    FITNESS_DATA currentRecord;
+
+    for (int i = 0; i < numRecords; i++)
+    {
+        char date[11], time[6], steps[6];
+        fgets(lineBuffer, maxLength, file);
+        tokeniseRecord(lineBuffer, ",", date, time, steps);
+
+        strcpy(currentRecord.date, date);
+        strcpy(currentRecord.time, time);
+
+        currentRecord.steps = atoi(steps);
+
+        fitnessData[i] = currentRecord;
+
+        // Print first 3 records
+        if (i < 3)
+        {
+            printf("%s/%s/%d\n", date, time, atoi(steps));
+        }
+    }
+
+    return 0;
 }
